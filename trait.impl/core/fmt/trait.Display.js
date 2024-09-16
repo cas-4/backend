eb443302fe7991997cc1444932e96d04b2db6b0a
boozec <boozec@users.noreply.github.com>
2024-09-16 20:41:59 +0000

    deploy: 99bb59373b5918b2baf7e70f510d2b0ec1122a28

---
+++ trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"cas":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"cas/errors/enum.AppError.html\" title=\"enum cas::errors::AppError\">AppError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
