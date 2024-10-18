251750d598645daed71bc74fe452ee7d619d63f4
boozec <boozec@users.noreply.github.com>
2024-10-18 13:09:43 +0000

    deploy: 874e85a8b1a6c5d9c5bbc1f4429785563a1f7426

--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["cas",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"cas/errors/enum.AppError.html\" title=\"enum cas::errors::AppError\">AppError</a>"]]]]);
+    var implementors = Object.fromEntries([["cas",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"cas/errors/enum.AppError.html\" title=\"enum cas::errors::AppError\">AppError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"cas/graphql/types/position/enum.MovingActivity.html\" title=\"enum cas::graphql::types::position::MovingActivity\">MovingActivity</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[265]}
+//{"start":57,"fragment_lengths":[573]}
