1ae3487887db39e9c77cc2fc8458519a52fde686
boozec <boozec@users.noreply.github.com>
2025-01-17 14:00:41 +0000

    deploy: c6dcfcfe833c28fec288918965fc31cd86199fb1

--- trait.impl/axum_core/extract/trait.FromRequestParts.js
+++ trait.impl/axum_core/extract/trait.FromRequestParts.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["cas",[["impl&lt;S&gt; FromRequestParts&lt;S&gt; for <a class=\"enum\" href=\"cas/graphql/types/jwt/enum.Authentication.html\" title=\"enum cas::graphql::types::jwt::Authentication\">Authentication</a><div class=\"where\">where\n    S: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a>,</div>"]]]]);
+    var implementors = Object.fromEntries([["cas",[["impl&lt;S&gt; FromRequestParts&lt;S&gt; for <a class=\"enum\" href=\"cas/graphql/types/jwt/enum.Authentication.html\" title=\"enum cas::graphql::types::jwt::Authentication\">Authentication</a><div class=\"where\">where\n    S: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
