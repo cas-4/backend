3fd10f0dc5af0f1a57508300ff97789a439af392
boozec <boozec@users.noreply.github.com>
2025-02-03 16:06:39 +0000

    deploy: 8b1035749e927087976e0e30576622c59b87e08f

--- trait.impl/core/str/traits/trait.FromStr.js
+++ trait.impl/core/str/traits/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["cas",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"cas/graphql/types/notification/enum.LevelAlert.html\" title=\"enum cas::graphql::types::notification::LevelAlert\">LevelAlert</a>"]]]]);
+    var implementors = Object.fromEntries([["cas",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"cas/graphql/types/notification/enum.LevelAlert.html\" title=\"enum cas::graphql::types::notification::LevelAlert\">LevelAlert</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
